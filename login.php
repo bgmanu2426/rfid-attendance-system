95980880b78c7ee3a0240a91f997595a25337d27
Manubg <bgmanu2426@gmail.com>
2023-04-23 21:12:13 +0530

    alerts style updated

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['userNumber'] = $user_number;
             $_SESSION['user'] = true;
             $_SESSION['admin'] = true;
-            header("location: logs.php");
+            header("location: manageUsers.php");
         } else {
             $showError = true;
         }
@@ -73,13 +73,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Alerts -->
     <?php
     if ($showError) {
-        echo '<div class="alert alert-danger fade show" style="position: absolute; left: 560px; top: 80px;" role="alert"><strong>Error!</strong> Invalid Credentials</div>';
+        echo '
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </symbol>
+        </svg>
+        <div class="alert alert-danger d-flex align-items-center" role="alert" style="height: 48px; margin-bottom: 0; position: absolute; left: 40vw; top:59px;">
+            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:" style="width: 18px;">
+                <use xlink:href="#exclamation-triangle-fill"></use>
+            </svg>
+            <div>
+                Invalid Credentials
+            </div>
+        </div>
+      ';
     }
     ?>
 
     <!-- Login form -->
     <div class="container col-md-6 my-5" style="min-height: 66.5vh;">
-        <h3 class="text-center m-4">Login **title**</h3>
+        <h3 class="text-center mt-5 pt-2 mb-3">Login **title**</h3>
         <form action="./login.php" method="POST">
             <div class="form-floating mb-3">
                 <input type="text" class="form-control my-4" id="user_number" name="user_number" placeholder="name@example.com">
@@ -114,7 +128,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // To make the alert auto dismissable
         $(document).ready(function() {
-            $(".alert").fadeTo(2500, 400).slideUp(400, function() {
+            $(".alert").fadeTo(3000, 1).slideUp(300, function() {
                 $(this).alert('close');
             })
         })
--- manageUsers.php
+++ manageUsers.php
@@ -128,9 +128,37 @@ if (isset($_GET['delete'])) {
     require "./apps/editModal.php";
     require "./apps/deleteModal.php";
     if ($update) {
-        echo '<div class="alert alert-success fade show" role="alert"><strong>Updated!</strong> Student details has been updated successfully</div>';
+        echo '
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" viewBox="0 0 16 16">
+                <path
+                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+            </symbol>
+        </svg>
+        <div class="alert alert-success d-flex align-items-center" role="alert" style="height: 48px; margin-bottom: 0; position: absolute; left: 37.5vw; top:59px;">
+            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:" style="width: 18px">
+                <use xlink:href="#check-circle-fill" />
+            </svg>
+            <div>
+                Student details have been updated
+            </div>
+        </div>';
     } elseif ($delete) {
-        echo '<div class="alert alert-danger fade show" role="alert"><strong>Deleted!</strong> Student details has been deleted successfully</div>';
+        echo '
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" viewBox="0 0 16 16">
+                <path
+                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+            </symbol>
+        </svg>
+        <div class="alert alert-danger d-flex align-items-center" role="alert" style="height: 48px; margin-bottom: 0; position: absolute; left: 37.5vw; top:59px;">
+            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:" style="width: 18px">
+                <use xlink:href="#check-circle-fill" />
+            </svg>
+            <div>
+                Student details have been deleted
+            </div>
+        </div>';
     }
     ?>
 
