7eaecc21d7796b2875721fa5139aee47500bcdf6
Lakshminarayana <106466608+bgmanu2426@users.noreply.github.com>
2023-11-22 13:08:00 +0530

    Update secrets.h

--- public/rfidattendance/secrets.h
+++ public/rfidattendance/secrets.h
@@ -1,4 +1,4 @@
 const char WLAN_USERNAME[] = "Manu BG";
 const char WLAN_PASSWORD[] = "Manu@7795";
 
-String REQ_URL = "http://3.234.15.228/getdata.php";
+String REQ_URL = "http://localhost/rfidattendance/getdata.php";
